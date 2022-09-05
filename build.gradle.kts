fc2f6976b478706ae1400ddd9f1f53579123d2c7
cocodori <moradulee@gmail.com>
2022-09-06 00:39:59 +0900

    feat: 테스트 환경 구성

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,10 @@ subprojects {
         implementation("org.springframework.boot:spring-boot-starter")
         implementation("org.jetbrains.kotlin:kotlin-reflect")
         implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
         testImplementation("org.springframework.boot:spring-boot-starter-test")
+        testImplementation("io.mockk:mockk:1.12.7")
+        testImplementation("com.ninja-squad:springmockk:3.1.1")
     }
 
     tasks.getByName("bootJar") {
